75ed2f4e76f914c66e40401cff8b695d78b3849a
Peter Craig <pac4607@rit.edu>
2023-05-04 09:59:04 -0400

    Added comments, and began reorganizing the code

--- Moving_Group_Algorithm.py
+++ Moving_Group_Algorithm.py
@@ -6,27 +6,43 @@ from scipy.ndimage.filters import gaussian_filter
 import matplotlib.patches as patches
 from shapely.geometry import Point, Polygon
 
+'''
+This module contains code for my algorithm to detect moving grouos
+This is an updated version of the method described in Craig et. al. 2021
+Written by Peter Craig
+5/4/23
+'''
+
+
 def PolyArea(x,y):
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
 def interpolate_img_data(img,xvals,yvals, rescale_factor):
+
+	'''
+	Uses bilinear spline interpolators to increase image resolution
+	Takes in a 2xN numpy array
+	xvals is an array containing the x positions for img
+	yvals is an array containing the y positions for img
+	rescale_factor sets the dimensions of the new image. New shape = old shape * rescale_factor
+	'''
 
 	interp_func = interp.RectBivariateSpline(xvals,yvals,img,s=0.1)
 
 	new_im = np.zeros( (img.shape[0]  * rescale_factor , img.shape[1] * rescale_factor) )
 	nx = np.arange(min(xvals) , max(xvals) , (  xvals[1] - xvals[0] ) / rescale_factor)
 	ny = np.arange(min(yvals) , max(yvals) , (  yvals[1] - yvals[0] ) / rescale_factor)
-	print (interp_func(nx,ny).shape)
-	plt.subplot(2,1,1)
-	plt.imshow(np.transpose(interp_func(nx,ny)))
-	plt.title("New")
-	plt.subplot(2,1,2)
-	plt.title("Old")
-	plt.imshow(np.transpose(img))
-	plt.show()
-	
+
 	return np.transpose(interp_func(nx,ny)) , nx , ny
+
 def get_test_data():
+
+	'''
+	generates sample data
+	will include backround data plus some artificial moving groups
+	designed for algorithm testing
+	'''
+
 
 	##Background first
 
@@ -63,16 +79,47 @@ def get_test_data():
 
 	return x,y
 
+def get_backround(x,y,bw = 3 , N = 10):
 
-
-def find_moving_groups(x,y):
-
+	'''
+	returns the background pixel data for the given data set
+	bw is the bin width
+	N determines how accurate the integration is
+	'''
+	x1 = np.arange(int( np.min(x) )  ,  int( np.max(x) + 1 )  , bw)
+	y1 = np.arange(int( np.min(y) )  ,  int( np.max(y) + 1 )  , bw)
 	
 	gx1 = np.mean(x)
 	gy1 = np.mean(y)
 	std_x1 = np.std(x)
 	std_y1 = np.std(y)
-	bw = 3
+	
+	xgrid , ygrid = np.meshgrid(x1,y1)
+
+	xstep = x1[1] = x1[0]
+	ystep = y1[1] - y1[0]
+
+	
+	pos = np.dstack((xgrid, ygrid))
+
+	pdf = stats.multivariate_normal(mean = [gx1,gy1] , cov = [ [ std_x1 ** 2 , 0 ] , [0 , std_y1 ** 2] ] )
+
+	res = pdf.pdf(pos) * len(x) * bw ** 2
+	return res
+
+
+def find_moving_groups(x,y,bw = 3):
+
+	
+	'''
+	This is the main algorithm
+	x and y are our velocity components.
+	This will typically be v_r and v_phi
+	The algorithm should work given any two equal length arrays however
+	'''
+
+
+	
 	rescale = 5
 
 	x1 = np.arange(int( np.min(x) )  ,  int( np.max(x) + 1 )  , bw)
@@ -87,27 +134,10 @@ def find_moving_groups(x,y):
 
 	imd = np.transpose(statres[0])
 	
-	
-	x1 = np.arange(int( np.min(x) )  ,  int( np.max(x) + 1 )  , bw)
-	y1 = np.arange(int( np.min(y) )  ,  int( np.max(y) + 1 )  , bw)
-	
-	
-
-	
-	xgrid , ygrid = np.meshgrid(x1,y1)
-
-	xstep = x1[1] = x1[0]
-	ystep = y1[1] - y1[0]
-
-	
-	pos = np.dstack((xgrid, ygrid))
-
-	pdf = stats.multivariate_normal(mean = [gx1,gy1] , cov = [ [ std_x1 ** 2 , 0 ] , [0 , std_y1 ** 2] ] )
-
-	res = pdf.pdf(pos) * len(x) * bw ** 2
-	
+	res = get_backround(x,y,bw)
 	residual = imd - res
 	ii = np.where( (abs(residual) > 1))
+	
 	
 	cutoff = np.std(residual[ii])
 	residual /= cutoff
@@ -133,7 +163,7 @@ def find_moving_groups(x,y):
 
 	plt.pcolormesh(nxgrid , nygrid , residual)
 	plt.colorbar()
-	cs = plt.contour(nxgrid,nygrid , residual , levels = [3])
+	cs = plt.contour(nxgrid,nygrid , residual , levels = [2])
 	print (cs.collections[0].get_paths()[0])
 	good_x = []
 	good_y = []
@@ -154,7 +184,7 @@ def find_moving_groups(x,y):
 		print (len(residual[in2]))
 		area = np.sum(residual[in2]) * bw / rescale
 		print (area , np.mean(x) , np.mean(y))
-		if area > 75:
+		if area > 90:
 			paths.append(p)
 			good_x.append(np.mean(x))
 			good_y.append(np.mean(y))
@@ -181,14 +211,7 @@ def find_moving_groups(x,y):
 		patch = patches.PathPatch(p, facecolor='black', lw=2 , fill = False)
 		ax.add_patch(patch)
 	plt.show()
-	exit()
-	residual = residual.flatten()
-	xgrid = nxgrid.flatten()
-	ygrid = nygrid.flatten()
 
-	ii = np.argmax(residual)
-	print (xgrid[ii] , ygrid[ii] , residual[ii])
-	
 
 
 x,y = get_test_data()
