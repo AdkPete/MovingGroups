abfe5235866d67bbf09bc67b8ede1e15a038e629
Peter Craig <pac4607@rit.edu>
2023-06-02 09:58:55 -0400

    Cleaned up code, added docstring

--- Moving_Group_Algorithm.py
+++ Moving_Group_Algorithm.py
@@ -1,3 +1,13 @@
+
+'''
+This module contains code for our algorithm to detect moving grouos
+This is an updated version of the method described in Craig et. al. 2021
+Written by Peter Craig
+5/4/23
+'''
+
+##Useful imports
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
@@ -9,35 +19,13 @@ from astroquery.gaia import Gaia
 import astropy.units as u
 import astropy.coordinates as coord
 import os
-
-'''
-This module contains code for my algorithm to detect moving grouos
-This is an updated version of the method described in Craig et. al. 2021
-Written by Peter Craig
-5/4/23
-'''
-
-
 def PolyArea(x,y):
-    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
-def interpolate_img_data(img,xvals,yvals, rescale_factor):
-
-	'''
-	Uses bilinear spline interpolators to increase image resolution
-	Takes in a 2xN numpy array
-	xvals is an array containing the x positions for img
-	yvals is an array containing the y positions for img
-	rescale_factor sets the dimensions of the new image. New shape = old shape * rescale_factor
-	'''
-
-	interp_func = interp.RectBivariateSpline(xvals,yvals,img,s=0.1)
-
-	new_im = np.zeros( (img.shape[0]  * rescale_factor , img.shape[1] * rescale_factor) )
-	nx = np.arange(min(xvals) , max(xvals) , (  xvals[1] - xvals[0] ) / rescale_factor)
-	ny = np.arange(min(yvals) , max(yvals) , (  yvals[1] - yvals[0] ) / rescale_factor)
-
-	return np.transpose(interp_func(nx,ny)) , nx , ny
+	##Simple function to compute the area of a polygon given the
+	##x and y positions of the vertices.
+	##x and y are both 1d arrays containing the x,y coordinates for
+	##the vertices, respectively.
+	return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
 def get_test_data():
 
@@ -53,7 +41,7 @@ def get_test_data():
 	bx = np.random.normal(0 , 50 , 50000)
 	by = np.random.normal(0,50,50000)
 
-	##Groups
+	##Add in Simulated groups
 	
 	gx = np.random.normal(25 , 5 , 350)
 	gy = np.random.normal(25 , 5 , 350)
@@ -83,13 +71,17 @@ def get_test_data():
 
 	return x,y
 
-def get_background(x,y,bw = 3 , N = 20):
+def get_gaussian_background(x , y , params):
 
 	'''
-	returns the background pixel data for the given data set
-	bw is the bin width
-	N determines how accurate the integration is
+	Returns the background pixel data for the given data set
+	Assumes a Gaussian distribution
+	x and y are our two velocity components
 	'''
+
+	bw = params["bin_width"]
+	N = params["gauss_integration_N"]
+
 	x1 = np.arange(int( np.min(x) )  ,  int( np.max(x) + 1 )  , bw / N)
 	y1 = np.arange(int( np.min(y) )  ,  int( np.max(y) + 1 )  , bw / N)
 	
@@ -106,7 +98,7 @@ def get_background(x,y,bw = 3 , N = 20):
 
 	##Res evaluates the pdf at our increased resolution
 	res = pdf.pdf(pos)
-	##Now we integrate down to our original image resolution
+	##Now we integrate down to our original bin sizes
 
 	##Set bin edges
 	x1 = np.arange(int( np.min(x) )  ,  int( np.max(x) + 1 )  , bw)
@@ -121,23 +113,83 @@ def get_background(x,y,bw = 3 , N = 20):
 
 	return background
 
-def get_smoothed_background(imd,xgrid,ygrid):
+def get_smoothed_background(imd,xgrid,ygrid , params):
 
+	'''
+	Function to return a smoothed background distribution
+	This is done using a gaussian smoothing kernel
+	The size of this kernel is set with params["background_smoothing_sigma"]
+	'''
 
-
+	smooth_sigma = params["background_smoothing_sigma"]
 	
-	new_imd = gaussian_filter(imd , sigma = 5)
-	plt.subplot(1,2,1)
-	plt.title("New")
-	plt.pcolormesh(xgrid , ygrid , new_imd)
+	new_imd = gaussian_filter(imd , sigma = smooth_sigma)
 
-	plt.subplot(1,2,2)
-	plt.title("old")
-	plt.pcolormesh(xgrid , ygrid , imd)
-	plt.show()
+	if params["show_smoothing_figures"]:
+		plt.subplot(1,2,1)
+		plt.title("New")
+		plt.pcolormesh(xgrid , ygrid , new_imd)
+
+		plt.subplot(1,2,2)
+		plt.title("old")
+		plt.pcolormesh(xgrid , ygrid , imd)
+		plt.show()
+
 	return new_imd
 
-def find_moving_groups(x,y,bw = 3,showplots = False,verbose=False,sigma = 2 , areacut = None):
+def get_DR3_sample(N = 50000 , vsun = 220):
+
+	'''
+	This function will query Gaia DR3 to get a solar neighborhood sample
+	The query can be relatively slow, so we save the results to a file (gaia.npy) and then
+	automatically retrieve from there on subsequent runs.
+
+	Note: It may be worth updating this query to implement error cuts to improve data quality
+	Some cuts on parralax/error, rv errors and proper motion errors or altered distance cuts may provide
+	better results.
+
+	N is the maximum number of search results to return.
+	xsun sets the solar y velocity, which gets subtracted off of the y velocities.
+	This should not effect the algorithm as it just shifts the data.
+
+	Returns
+	___________
+	returns the x and y velocity components for a sample of nearby Gaia sources.
+
+	'''
+
+	if os.path.exists("gaia.npy"): ##Read from file if possible
+		x , y = np.load("gaia.npy" , allow_pickle = True)
+		return x , y
+
+
+	##Runs our query with cuts on parallax, plus we require RVs to exist
+
+	query = "select top {} * from gaiadr3.gaia_source where ".format(N)
+	
+	##Cuts go here:
+	query += "parallax > 10 and radial_velocity is not NULL"
+
+	##Run query
+	job = Gaia.launch_job_async(query)
+	r = job.get_results()
+	print ("Number of stars returned {}".format(len(r)))
+
+	##Convert to Galactocentric coordinates w/ astropy
+	sc = coord.SkyCoord(ra = r["ra"] , dec = r["dec"] , distance = 1 * u.kpc / r["parallax"].value , pm_ra_cosdec = r["pmra"] , pm_dec = r["pmdec"], radial_velocity = r["radial_velocity"])
+
+
+	gc = sc.transform_to(coord.Galactocentric)
+	
+	np.save("gaia.npy" , [gc.v_x.value , gc.v_y.value - 220])
+	##These are cartesian velocity compontents, commonly used in moving group analysis
+	##Some coordinate definitions flip the sign of the x velocities. Doesn't change anything
+	##except that it will flip the distribution in x (and therefore the group positions)
+
+	return gc.v_x.value , gc.v_y.value - 220
+
+
+def find_moving_groups(x,y, sigma , params):
 
 	
 	'''
@@ -145,16 +197,33 @@ def find_moving_groups(x,y,bw = 3,showplots = False,verbose=False,sigma = 2 , ar
 	x and y are our velocity components.
 	This will typically be v_r and v_phi
 	The algorithm should work given any two equal length arrays however
+	sigma sets the minimum value considered to be above the noise
+	params is a dictionary containing all other parameters, see below.
+	
+	Returns
+	____________
+	good_x : array with x positions of the detected groups
+	good_y : array with y positions of the detected groups
+	residual : 2d array with residual data
+	paths : array containing contour data
+	xgrid : x bin edges
+	ygrid : y bin edges
+	imd : 2d array containing binned velopcity data
+
+	The last three arguments are primarilly intended for making plots.
 	'''
 
-	if areacut == None:
-		areacut = 2 * sigma
-
+	areacut = params["area_cutoff"]
+	bw = params["bin_width"]
+	verbose = params["verbose"]
 	
 	rescale = 5
 
 	x1 = np.arange(int( np.min(x) )  ,  int( np.max(x) + 1 )  , bw)
 	y1 = np.arange(int( np.min(y) )  ,  int( np.max(y) + 1 )  , bw)
+
+	##Add an extra bin to the end, we want to define bin edges and this is required to
+	##get the right shape of the residual array
 
 	x1 = np.append(x1 , x1[-1] + bw)
 	y1 = np.append(y1 , y1[-1] + bw)
@@ -165,8 +234,12 @@ def find_moving_groups(x,y,bw = 3,showplots = False,verbose=False,sigma = 2 , ar
 
 	imd = np.transpose(statres[0])
 	
-	res = get_smoothed_background(imd,xgrid,ygrid)
+	
+	if params["background_type"] == "Smoothed":
+		res = get_smoothed_background(imd,xgrid,ygrid,params)
 
+	elif params["background_type"] == "Gaussian":
+		res = get_gaussian_background(x,y,params)
 
 	sigma_grid = np.ones(res.shape)
 
@@ -178,22 +251,15 @@ def find_moving_groups(x,y,bw = 3,showplots = False,verbose=False,sigma = 2 , ar
 
 	residual /= sigma_grid
 
-	#ii = np.where( (abs(residual) > 1))
-	
-	
-
-	#cutoff = np.std(residual[ii])
-	#residual /= cutoff
 
 	x1 = np.arange(int( np.min(x) )  ,  int( np.max(x) + 1 )  , bw)
 	y1 = np.arange(int( np.min(y) )  ,  int( np.max(y) + 1 )  , bw)
-	#residual , nx , ny = interpolate_img_data(np.transpose(residual) , x1 , y1 , rescale)
+	
 	nx,ny = x1,y1
 	nxgrid , nygrid = np.meshgrid(nx,ny)
-	if showplots:
-		
 
-
+	if params["display_residuals"]:
+		##Plots residual functions
 
 		plt.pcolormesh(nxgrid , nygrid , residual)
 		plt.colorbar()
@@ -232,95 +298,82 @@ def find_moving_groups(x,y,bw = 3,showplots = False,verbose=False,sigma = 2 , ar
 			good_y.append(np.mean(y))
 
 
-	if not showplots:
-		plt.close()
-		return good_x,good_y
+
+	return good_x , good_y , residual , paths , xgrid , ygrid , imd
+
+def plot_groups(imd,paths,xgrid,ygrid,show=False):
+
+
+
+	fig, ax = plt.subplots()
+	plt.pcolormesh(xgrid , ygrid , imd)
+	plt.colorbar()
+	plt.scatter(good_x , good_y , marker = "x" , s = 10 , color = "red")
+	for p in paths:
+		patch = patches.PathPatch(p, facecolor='black', lw=2 , fill = False)
+		ax.add_patch(patch)
 	
-	plt.scatter(good_x , good_y , marker = "x" , s = 10 , color = "red")
-	plt.show()
+	plt.xlim(params["plot_x_limits"])
+	plt.ylim(params["plot_y_limits"])
+	plt.xlabel("U (km/s)")
+	plt.ylabel("V (km/s)")
 
-	fig, ax = plt.subplots()
-	plt.pcolormesh(nxgrid , nygrid , residual)
-	plt.colorbar()
-	plt.scatter(good_x , good_y , marker = "x" , s = 10 , color = "red")
-	for p in paths:
-		patch = patches.PathPatch(p, facecolor='black', lw=2 , fill = False)
-		ax.add_patch(patch)
-	plt.show()
+	plt.savefig("Moving_Groups.pdf")
+	if show:
+		plt.show()
+
 
 	fig, ax = plt.subplots()
 	plt.pcolormesh(xgrid , ygrid , imd)
 	plt.colorbar()
-	plt.scatter(good_x , good_y , marker = "x" , s = 10 , color = "red")
-	for p in paths:
-		patch = patches.PathPatch(p, facecolor='black', lw=2 , fill = False)
-		ax.add_patch(patch)
-	plt.xlim(-50,50)
-	plt.ylim(-50,50)
+	plt.scatter(good_x , good_y , color = "red" , marker = "x")
+	plt.xlim(params["plot_x_limits"])
+	plt.ylim(params["plot_y_limits"])
 	plt.xlabel("U (km/s)")
 	plt.ylabel("V (km/s)")
-	plt.savefig("Moving_Groups.png")
-	plt.show()
-
-	fig, ax = plt.subplots()
-	plt.pcolormesh(xgrid , ygrid , imd)
-	plt.colorbar()
-
-	plt.xlim(-50,50)
-	plt.ylim(-50,50)
-	plt.xlabel("U (km/s)")
-	plt.ylabel("V (km/s)")
-	plt.savefig("Moving_Groups_ng.png")
-	plt.show()
+	plt.savefig("Moving_Groups_Scatter.pdf")
+	if show:
+		plt.show()
 
 	return good_x , good_y
 
-def get_DR3_sample():
+if __name__ == "__main__":
 
-	if os.path.exists("gaia.npy"):
-		x , y = np.load("gaia.npy" , allow_pickle = True)
-		return x , y
-	job = Gaia.launch_job_async("select top 50000 * from gaiadr3.gaia_source where parallax > 10 and radial_velocity is not NULL")
-	r = job.get_results()
-	print ("Number of stars returned {}".format(len(r)))
-	sc = coord.SkyCoord(ra = r["ra"] , dec = r["dec"] , distance = 1 * u.kpc / r["parallax"].value , pm_ra_cosdec = r["pmra"] , pm_dec = r["pmdec"], radial_velocity = r["radial_velocity"])
+	##First, set up parameter dictionary
 
-	gc = sc.transform_to(coord.Galactocentric)
-	
-	np.save("gaia.npy" , [gc.v_x.value , gc.v_y.value - 220])
-	return gc.v_x.value , gc.v_y.value - 220
+	params = {}
 
-def filter(x,y):
-	ii = np.where( (abs(x) < 100) & (abs(y) < 100))
-	x = x[ii]
-	y = y[ii]
-	return x,y
+	##Bin width used for binning velocity data
+	params["bin_width"] = 3
 
-x,y = get_DR3_sample()
-x,y = filter(x,y)
+	##Number of integration steps to use for Gaussian background (uses N^2 positions per bin)
+	params["gauss_integration_N"] = 30
 
-plt.hist2d(x,y , bins = 75)
-plt.xlim(-50,50)
-plt.ylim(-50,50)
-plt.xlabel("U (km/s)")
-plt.ylabel("V (km/s)")
-plt.savefig("GaiaVelocities.pdf")
-plt.close()
-#x,y = get_test_data()
-find_moving_groups(x,y , showplots = True,verbose=True,sigma = 2.5 )
+	##area_cutoff sets the detection threshold. The area is computed as a surface integral of 
+	##the residual / sqrt(background) inside each contour. If this is less then area_cutoff, the 
+	##group is rejected. Typical values are something like 5 * bin_width ^ 2
+	params["area_cutoff"] = 5
 
-#find_moving_groups(x,y , showplots = True,verbose=True,sigma = 11 , areacut = 10)
-#find_moving_groups(x,y , bw = e , showplots = True,verbose=True,sigma = 7.25 , areacut = 7.5)
-'''
-ng = []
-for i in tqdm(range(50)):
+	##if verbose, we print out some extra info, mostly for debugging purposes
+	params["verbose"] = False
+
+	##Set size of smoothing kernel for smoothed background. 
+	params["background_smoothing_sigma"] = 20
+
+	##If true, display the smoothinf before/after figures. Useful for testing out smoothing lengths
+	params["show_smoothing_figures"] = False
+
+	##Set to either 'Gaussian' or 'Smoothed'. Determines which kind of background to use
+	params["background_type"] = "Gaussian"
+
+	##if true, display residual figure
+	params["display_residuals"] = True
+
+	##if true, make figures in find_moving_groups. Otherwise we skip this step.
+	params["make_group_figures"] = True
+
+
 	x,y = get_test_data()
-	gx , gy = find_moving_groups(x,y)
 
-	ng.append(len(gx))
+	good_x , good_y , residual , paths , xgrid , ygrid , imd = find_moving_groups(x,y,3,params)
 
-if min(ng) < 4 or max(ng) > 4:
-	plt.hist(ng)
-	plt.show()
-print (min(ng) , max(ng))
-'''
